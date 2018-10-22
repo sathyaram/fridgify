a68e0b516183ccd286fa216d830c0968d9d1d53f
Sathya Ram <sathyatheram@gmail.com>
2018-10-22 17:37:18 -0400

    got fridge open and closing animation to work!

--- frontend/src/Refrigerator/Refrigerator.js
+++ frontend/src/Refrigerator/Refrigerator.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './Refrigerator.css';
 
 class Refrigerator extends Component {
+
+  openDoor() {
+    var d = document.querySelector('.refrigerator');
+    d.classList.toggle("open");
+  }
+
   render() {
     return (
       <main>
-        <div className="refrigerator">
-          <div className="handle"></div>
+        <div onClick={this.openDoor} className="refrigerator">
+          <div className="backDoor">
+            Testio!
+            <div className="handle"></div>
+          </div>
+          
         </div>
       </main>
     );
