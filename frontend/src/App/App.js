8be80fc46a19afe19fd07c9515a18591f0f990d9
Sathya Ram <sathyatheram@gmail.com>
2018-10-22 17:00:01 -0400

    created refrig component, and started preliminary styling

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import Refrigerator from '../Refrigerator/Refrigerator'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        <header>
-          
-        </header>
+      <div>
+      <header>
+        <h1>Fridgify</h1>
+        <button>+</button>
+      </header>
+      <Refrigerator />
+      </div>
     );
   }
 }
---
+++ frontend/src/Refrigerator/Refrigerator.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Refrigerator.css';
+
+class Refrigerator extends Component {
+  render() {
+    return (
+      <main>
+        <div className="refrigerator">
+          <div className="handle"></div>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Refrigerator;
