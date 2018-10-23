a30b7613fcc8b52edbe260e30810ec2b49a59df0
Sathya Ram <sathyatheram@gmail.com>
2018-10-23 09:56:41 -0400

    cleaned up some files in the backend

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,15 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-
-// set up express app
-const app = express()
-
-// connect to mongoDB
-if (process.env.NODE_ENV === 'production') {
-  mongoose.connect(process.env.MLAB_URL, {useMongoClient: true})
-} else {
-  mongoose.connect('mongodb://localhost/fridgify', {useMongoClient: true})
-}
-mongoose.Promise = global.Promise
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'));
 
 app.use('/api', require('./routes/api'))
 
-// listen for requests
-app.listen(process.env.port || 3001, function () {
-  console.log('Listening on port 3001')
+// Heroku
+app.set('port', process.env.PORT || 8008)
+
+app.listen(app.get('port'), () => {
+  console.log('up and running')
 })
