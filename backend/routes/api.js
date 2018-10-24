08630afe48c67835c7a6cb7bc48cc8ef2bde379c
Marichka Tsiuriak <marichka.ts94@gmail.com>
2018-10-24 10:53:05 -0400

    add routes for Category

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,15 +3,33 @@ const router = express.Router()
 const Item = require('../db/models/Item')
 const Category = require('../db/models/Category')
 
+// router.get('/',(req, res) => {
+//   res.sendFile(__dirname + '../../frontend/build/index.html')
+// })
+
+// GET for Categories
+router.get('/api/categories', (req, res, next) => {
+  Category.find().then((items) => {
+    res.json(items)
+  }).catch(next)
+})
+
+// temporary POST for Categories
+router.post('/api/categories', (req, res, next) => {
+  Category.create(req.body).then(category => {
+    res.send(category)
+  }).catch(next)
+})
+
 // get a list of items from the db
-router.get('/items', (req, res, next) => {
+router.get('/api/items', (req, res, next) => {
   Item.find().then((items) => {
     res.json(items)
-  })
+  }).catch(next)
 })
 
 // add a new item to the db
-router.post('/items', (req, res, next) => {
+router.post('/api/items', (req, res, next) => {
   console.log('adding an item');
   console.log(req.body);
   Item.create(req.body).then(item => {
@@ -20,7 +38,7 @@ router.post('/items', (req, res, next) => {
 })
 
 // update the item in the db
-router.put('/items/:id', (req, res, next) => {
+router.put('/api/items/:id', (req, res, next) => {
   Item.findByIdAndUpdate({_id: req.params.id}, req.body).then(() => {
     Item.findOne({_id: req.params.id}).then(item => {
       res.send(item)
@@ -29,7 +47,7 @@ router.put('/items/:id', (req, res, next) => {
 })
 
 // delete an item from db
-router.delete('/items/:id', (req, res, next) => {
+router.delete('/api/items/:id', (req, res, next) => {
   Item.findByIdAndRemove({_id: req.params.id}).then(item => {
     res.send(item)
   })
